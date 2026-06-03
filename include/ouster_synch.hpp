b9b3ae66e79edf64794f238d9d90f86fdea96fed
Adam <adam01.moumen@gmail.com>
2026-06-03 15:28:21 +0200

    Robust logic setup for ouster time synch.

--- include/ouster_synch.hpp
+++ include/ouster_synch.hpp
@@ -1,5 +1,6 @@
 #include <ros/ros.h>
 #include <sensor_msgs/PointCloud2.h>
+#include <deque>
 
 namespace ouster_synch
 {
@@ -18,17 +19,64 @@ namespace ouster_synch
         ros::Subscriber senti_lidar_ic;
         ros::Time senti_stamp;
         ros::Publisher updatedPointCloud;
-
+        std::deque<ros::Time> timestampList;
+        std::deque<sensor_msgs::PointCloud2> pointcloudList;
+        ros::Time rosTime;
+        sensor_msgs::PointCloud2 copiedPointCloud;
 
         void lidarSynchCallback(const sensor_msgs::PointCloud2ConstPtr &pointCloud)
         {
-            sensor_msgs::PointCloud2 copiedPointCloud = *pointCloud;
-            copiedPointCloud.header.stamp = senti_stamp;
-            updatedPointCloud.publish(copiedPointCloud);
+            //If we have a sentiboard time stamp, safely give it to received pointcloud
+            if(timestampList.size()>0)
+            {
+                copiedPointCloud = *pointCloud;
+                copiedPointCloud.header.stamp = timestampList.front();
+                timestampList.pop_front();
+                updatedPointCloud.publish(copiedPointCloud);
+            }
+            //If not, give warning and save last pointcloud.
+            else
+            {
+                ROS_WARN_STREAM("Warning, no sentiboard timestamp for current ouster lidar pointcloud exists.");
+                if(pointcloudList.size()==0)
+                {
+                    pointcloudList.push_back(*pointCloud);
+                    return;
+                }
+                else
+                {
+                    pointcloudList.pop_front();
+                    pointcloudList.push_back(*pointCloud);
+                    return;
+                }
+            
+            }
         }
         void sentiSynchCallback(const std_msgs::Header::ConstPtr &msg)
         {
-            senti_stamp = msg->stamp;
+            //senti_stamp = msg->stamp;
+            rosTime = ros::Time::now().toSec();
+            timestampList.push_back(msg->stamp);
+
+            if(pointcloudList.size()>0)
+            {
+                copiedPointCloud = pointcloudList.front();
+                copiedPointCloud = pointcloudList.pop_front();
+                if((rosTime - copiedPointCloud.header.stamp.toSec()) < 0.05)
+                {
+                    copiedPointCloud.header.stamp = timestampList.front();
+                    timestampList.pop_front();
+                    ROS_INFO_STREAM("Successfully gave a late sentiboard timestamp to a earlier received lidar pointcloud");
+                    updatedPointCloud.publish(copiedPointCloud);
+                    
+                }
+                
+                else
+                {
+                    ROS_WARN_STREAM("Warning: Earlier received lidar pointcloud and newly received sentiboard timestamp has too large difference in time. Throwing away pointcloud.")
+                }
+
+            }
         }
 
     };
